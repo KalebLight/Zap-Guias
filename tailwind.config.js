becc9d36582584fc2d1277f13bb30829c15c274d
KalebLight <kaleb.luiz00@gmail.com>
2024-11-13 10:46:52 -0300

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,24 @@ import forms from '@tailwindcss/forms';
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-    ],
-
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
-        },
+  content: [
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: '#192829',
+        primaryAnim: '#345254',
+        secondary: '#FF874F',
+        success: '#2ecc71',
+        danger: '#e74c3c',
+      },
     },
-
-    plugins: [forms],
+  },
+  plugins: [forms],
 };
