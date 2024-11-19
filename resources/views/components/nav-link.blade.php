1fde84945e8bd77a0c3a70e736387edf3adc2a6e
KalebLight <kaleb.luiz00@gmail.com>
2024-11-19 10:26:51 -0300

    fixing navlink text

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -2,8 +2,8 @@
 @php
     $classes =
         $active ?? false
-            ? 'inline-flex items-center text-primary p-2  sm:mx-6 font-bold'
-            : 'inline-flex font-light  sm:font-normal  sm:mx-6 items-center text-primary hover:bg-gray-100 transition duration-150 ease-in-out p-2';
+            ? 'inline-flex items-center text-primary p-2  sm:mx-2 font-bold text-xl'
+            : 'inline-flex font-light  sm:font-normal  sm:mx-2 items-center text-primary hover:bg-gray-100 transition duration-150 ease-in-out p-2 text-xl';
 @endphp
 <a {{ $attributes->merge(['class' => $classes]) }}>
     {{ $slot }}
