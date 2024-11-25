034fca6f78dfca53872937294609b4c459a6a751
KalebLight <kaleb.luiz00@gmail.com>
2024-11-25 01:19:01 -0300

    Update login.blade.php

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -5,8 +5,7 @@ use Illuminate\Support\Facades\Session;
 use Livewire\Attributes\Layout;
 use Livewire\Volt\Component;
 
-new #[Layout('layouts.guest')] class extends Component
-{
+new #[Layout('layouts.guest')] class extends Component {
     public LoginForm $form;
 
     /**
@@ -24,66 +23,66 @@ new #[Layout('layouts.guest')] class extends Component
     }
 }; ?>
 
-<div>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<div class="w-full flex justify-center mt-10 flex-col items-center">
+    <div class="md:w-[500px] w-[300px]">
+        <h1 class="text-4xl font-black text-primary mb-14">LOGIN</h1>
+    </div>
 
-    <form wire:submit="login">
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input wire:model="form.email" id="email" class="block mt-1 w-full" type="email" name="email" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('form.email')" class="mt-2" />
+    <form wire:submit.prevent="login" class="flex flex-col">
+        <!-- Botão Google -->
+        <div class="flex flex-col gap-4">
+            <button type="button"
+                class=" md:w-[500px] w-[300px] border border-black h-11 rounded-full hover:bg-gray-100">
+                <a href="/auth/google/redirect" class="flex items-center justify-center">
+                    <div class="flex items-center justify-center h-full">
+                        <img src="{{ asset('images/google-icon.png') }}" alt="Logo do Google" class="h-9 w-9">
+                    </div>
+                    <span class="font-light ml-3">Entrar com Google</span>
+                </a>
+            </button>
+
+            <!-- Botão Facebook -->
+
+            <button type="button"
+                class=" md:w-[500px] w-[300px] bg-facebook text-white h-11 rounded-full justify-center hover:bg-blue-600 ">
+                <a href="/auth/facebook/redirect" class="flex items-center justify-center">
+                    <div class="flex items-center justify-center h-full">
+                        <img src="{{ asset('images/facebook-icon.png') }}" alt="Logo do Facebook" class="h-9 w-9">
+                    </div>
+                    <span class="font-light ml-3">Entrar com Facebook</span>
+                </a>
+            </button>
         </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+        <p class="text-primary my-6 hidden md:block">
+            - - - - - - - - - - - - - - - - - - - - - - ou - - - - - - - - - - - - - - - - - - - - - -
+        </p>
+        <p class="text-primary my-6 block md:hidden">
+            - - - - - - - - - - - - - ou - - - - - - - - - - - - -
+        </p>
 
-            <x-text-input wire:model="form.password" id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
+        <div class="flex flex-col gap-4">
+            <x-text-input wire:model="form.email" id="email" placeholder='E-mail' class="block mt-1 w-full"
+                type="email" name="email" required autofocus autocomplete="username" />
+            <x-text-input wire:model="form.password" placeholder="Senha" id="password" class="block mt-1 w-full"
+                type="password" name="password" required autocomplete="current-password" />
 
             <x-input-error :messages="$errors->get('form.password')" class="mt-2" />
         </div>
-
-        <!-- Remember Me -->
-        <br>
-        <div class="w-full flex justify-center mx-2">
-             <a href="/auth/facebook/redirect" class="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 me-2 mb-2">
-                 <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 8 19">
-                     <path fill-rule="evenodd" d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z" clip-rule="evenodd"/>
-                 </svg>
-                 Sign in with Facebook
-             </a>
-        </div>
-        <div class="w-full flex justify-center mx-2">
-            <a href="/auth/google/redirect" class="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2 mb-2">
-                <svg class="w-4 h-4 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 19">
-                    <path fill-rule="evenodd" d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z" clip-rule="evenodd"/>
-                </svg>
-                Sign in with Google
-            </a>
-        </div>
-        
-        <div class="block mt-4">
-            <label for="remember" class="inline-flex items-center">
-                <input wire:model="form.remember" id="remember" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}" wire:navigate>
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
+        <a href="{{ route('password.request') }}" class="underline text-primary font-normal my-4" wire:navigate>Esqueci
+            minha
+            senha</a>
+        <div class="w-3/4">
+            <x-primary-button>
+                {{ __('Entrar') }}
             </x-primary-button>
         </div>
+
+
+
     </form>
+
+
+
+
 </div>
