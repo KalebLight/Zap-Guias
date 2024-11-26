cac51c4651c33a77bc16f479e22da184d4a8ff04
KalebLight <kaleb.luiz00@gmail.com>
2024-11-26 09:25:13 -0300

    Update register.blade.php

--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -8,8 +8,7 @@ use Illuminate\Validation\Rules;
 use Livewire\Attributes\Layout;
 use Livewire\Volt\Component;
 
-new #[Layout('layouts.guest')] class extends Component
-{
+new #[Layout('layouts.guest')] class extends Component {
     public string $name = '';
     public string $email = '';
     public string $password = '';
@@ -22,13 +21,13 @@ new #[Layout('layouts.guest')] class extends Component
     {
         $validated = $this->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'string', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $validated['password'] = Hash::make($validated['password']);
 
-        event(new Registered($user = User::create($validated)));
+        event(new Registered(($user = User::create($validated))));
 
         Auth::login($user);
 
@@ -36,53 +35,101 @@ new #[Layout('layouts.guest')] class extends Component
     }
 }; ?>
 
-<div>
-    <form wire:submit="register">
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input wire:model="name" id="name" class="block mt-1 w-full" type="text" name="name" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
+<div class="w-full flex justify-center">
+    <div>
+        <h2 class="text-primary font-black text-5xl">Junte-se à nossa</h2>
+        <h2 class="text-secondary font-black text-5xl mb-5">COMUNIDADE</h2>
+        <form wire:submit="register">
+            <!-- Name -->
+            <div class="flex flex-row">
+                <x-text-input wire:model="name" id="name" class="block mt-1 w-full" type="text" name="name"
+                    required autofocus autocomplete="name" placeholder='Nome Completo' />
+                <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                <p class="text-secondary text-2xl ml-1">*</p>
+            </div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+            <div class="mt-3 flex flex-row">
+                <x-text-input wire:model="name" id="name" class="block mt-1 w-full" type="text" name="name"
+                    required autofocus autocomplete="name" placeholder='CPF' />
+                <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                <p class="text-secondary text-2xl ml-1">*</p>
+            </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <div class="mt-3 flex flex-row">
+                <x-text-input wire:model="name" id="name" class="block mt-1 w-full" type="text" name="name"
+                    required autofocus autocomplete="name" placeholder='Número de Telefone' />
+                <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                <p class="text-secondary text-2xl ml-1">*</p>
+            </div>
 
-            <x-text-input wire:model="password" id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+            <div class="mt-3 flex flex-row">
+                <x-text-input wire:model="name" id="name" class="block mt-1 w-full" type="text" name="name"
+                    required autofocus autocomplete="name" placeholder='E-Mail' />
+                <x-input-error :messages="$errors->get('name')" class="mt-2" />
+                <p class="text-secondary text-2xl ml-1">*</p>
+            </div>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+            <div class="mt-3 flex flex-row">
+                <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email" name="email"
+                    required autocomplete="username" placeholder='Repita o E-Mail' />
+                <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                <p class="text-secondary text-2xl ml-1">*</p>
+            </div>
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+            <!-- Password -->
+            <div class="mt-3 flex flex-row">
 
-            <x-text-input wire:model="password_confirmation" id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
 
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
+                <x-text-input wire:model="password" id="password" class="block mt-1 w-full" type="password"
+                    name="password" required autocomplete="new-password" placeholder="Senha" />
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}" wire:navigate>
-                {{ __('Already registered?') }}
-            </a>
+                <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                <p class="text-secondary text-2xl ml-1">*</p>
+            </div>
 
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
+            <!-- Confirm Password -->
+            <div class="mt-3 flex flex-row ">
+
+                <x-text-input wire:model="password_confirmation" id="password_confirmation" class="block mt-1 w-full"
+                    type="password" name="password_confirmation" required autocomplete="new-password"
+                    placeholder="Repita a senha" />
+
+                <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
+                <p class="text-secondary text-2xl ml-1">*</p>
+            </div>
+
+            <div class="flex flex-row">
+                <p class="text-secondary text-2xl mr-2">*</p>
+                <p class="text-primary text-sm mt-2 font-medium">Campos obrigatórios</p>
+            </div>
+
+            <div class="space-y-4 mt-10">
+                <!-- Checkbox para termos de serviço e política de privacidade -->
+                <div class="flex items-center">
+                    <input type="checkbox" id="terms"
+                        class="w-4 h-4 text-secondary border-primary rounded focus:ring-secondary focus:ring-2 checked:bg-secondary checked:border-transparent">
+                    <label for="terms" class="ml-3 text-primary text-sm leading-none">
+                        Eu concordo com nossos
+                        <a href="#" class="text-secondary underline font-normal">Termos de Serviço</a>
+                        e
+                        <a href="#" class="text-secondary underline font-normal">Política de Privacidade</a>.
+                    </label>
+                </div>
+
+                <!-- Checkbox para receber notícias -->
+                <div class="flex items-center">
+                    <input type="checkbox" id="news"
+                        class="w-4 h-4 text-secondary border-primary rounded focus:ring-secondary focus:ring-2 checked:bg-secondary checked:border-transparent">
+                    <label for="news" class="ml-3 text-primary text-sm leading-none">
+                        Gostaria de receber notícias sobre viagens e o Turismo.
+                    </label>
+                </div>
+            </div>
+
+            <x-primary-button class="mt-10">
+                {{ __('Criar Conta') }}
             </x-primary-button>
-        </div>
-    </form>
+
+        </form>
+    </div>
 </div>
