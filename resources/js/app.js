5470ce4281a4db7af44f925d0ad7166f3f0dc43e
KalebLight <kaleb.luiz00@gmail.com>
2024-11-26 10:29:56 -0300

    adds cpf and phone input masks

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,27 @@
-import './bootstrap';
+import "./bootstrap";
+
+function applyCpfMask(input) {
+    let value = input.value.replace(/\D/g, ""); // Remove todos os caracteres não numéricos
+    value = value.replace(/(\d{3})(\d)/, "$1.$2"); // Adiciona o primeiro ponto
+    value = value.replace(/(\d{3})(\d)/, "$1.$2"); // Adiciona o segundo ponto
+    value = value.replace(/(\d{3})(\d{1,2})$/, "$1-$2"); // Adiciona o traço
+    input.value = value; // Atualiza o valor do input
+}
+
+function applyPhoneMask(input) {
+    let value = input.value.replace(/\D/g, ""); // Remove todos os caracteres não numéricos
+    value = value.replace(/(\d{2})(\d)/, "($1) $2"); // Adiciona os parênteses e o espaço
+    value = value.replace(/(\d{5})(\d)/, "$1-$2"); // Adiciona o traço
+    value = value.substring(0, 15); // Garante o máximo de 15 caracteres
+    input.value = value; // Atualiza o valor do input
+}
+
+const phoneInput = document.getElementById("phone");
+if (phoneInput) {
+    phoneInput.addEventListener("input", () => applyPhoneMask(phoneInput));
+}
+
+const cpfInput = document.getElementById("cpf");
+if (cpfInput) {
+    cpfInput.addEventListener("input", () => applyCpfMask(cpfInput));
+}
