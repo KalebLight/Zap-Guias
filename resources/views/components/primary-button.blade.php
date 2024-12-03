5bb36338c95529e4c6db5458eb81725c364168ce
KalebLight <kaleb.luiz00@gmail.com>
2024-12-03 09:12:05 -0300

    adding padding to primary button

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -1,8 +1,8 @@
 <button
     {{ $attributes->merge([
         'type' => 'submit',
-        'class' => 'custom-button xl:w-[570px] sm:w-[440px] w-[350px] lg:h-[50px]
-                     h-[36px] shadow-custom border border-primary rounded-full text-primary font-semibold hover:shadow-md hover:bg-secondary transition ease-in-out duration-150',
+        'class' =>
+            'custom-button xl:w-[570px] sm:w-[440px] w-[350px] lg:h-[50px] leading-none px-4 py-2 min-h-[36px] shadow-custom border border-primary rounded-full text-primary font-semibold hover:shadow-md hover:bg-secondary transition ease-in-out duration-150',
     ]) }}>
     {{ $slot }}
 </button>
