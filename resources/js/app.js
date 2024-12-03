3cbfc3dd5956039352f249ccf962af4af2b089e1
KalebLight <kaleb.luiz00@gmail.com>
2024-12-03 19:18:36 -0300

    add cnpj mask

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,24 +1,39 @@
 import "./bootstrap";
 
 function applyCpfMask(input) {
-    let value = input.value.replace(/\D/g, ""); // Remove todos os caracteres não numéricos
-    value = value.replace(/(\d{3})(\d)/, "$1.$2"); // Adiciona o primeiro ponto
-    value = value.replace(/(\d{3})(\d)/, "$1.$2"); // Adiciona o segundo ponto
-    value = value.replace(/(\d{3})(\d{1,2})$/, "$1-$2"); // Adiciona o traço
-    input.value = value; // Atualiza o valor do input
+    let value = input.value.replace(/\D/g, "");
+    value = value.replace(/(\d{3})(\d)/, "$1.$2");
+    value = value.replace(/(\d{3})(\d)/, "$1.$2");
+    value = value.replace(/(\d{3})(\d{1,2})$/, "$1-$2");
+    input.value = value;
 }
 
 function applyPhoneMask(input) {
-    let value = input.value.replace(/\D/g, ""); // Remove todos os caracteres não numéricos
-    value = value.replace(/(\d{2})(\d)/, "($1) $2"); // Adiciona os parênteses e o espaço
-    value = value.replace(/(\d{5})(\d)/, "$1-$2"); // Adiciona o traço
-    value = value.substring(0, 15); // Garante o máximo de 15 caracteres
-    input.value = value; // Atualiza o valor do input
+    let value = input.value.replace(/\D/g, "");
+    value = value.replace(/(\d{2})(\d)/, "($1) $2");
+    value = value.replace(/(\d{5})(\d)/, "$1-$2");
+    value = value.substring(0, 15);
+    input.value = value;
+}
+
+function applyCNPJMask(input) {
+    let value = input.value.replace(/\D/g, "");
+    value = value.replace(/(\d{2})(\d)/, "$1.$2");
+    value = value.replace(/(\d{5})(\d)/, "$1.$2");
+    value = value.replace(/(\d{8})(\d)/, "$1/$2");
+    value = value.replace(/(\d{12})(\d)/, "$1-$2");
+    value = value.substring(0, 18);
+    input.value = value;
 }
 
 const phoneInput = document.getElementById("phone");
 if (phoneInput) {
     phoneInput.addEventListener("input", () => applyPhoneMask(phoneInput));
+}
+
+const cnpjInput = document.getElementById("cnpj");
+if (cnpjInput) {
+    cnpjInput.addEventListener("input", () => applyCNPJMask(cnpjInput));
 }
 
 const cpfInput = document.getElementById("cpf");
