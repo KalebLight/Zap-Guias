a06ef037e04082f648e1602c4a1c1b9c0a0908c9
KalebLight <kaleb.luiz00@gmail.com>
2024-12-12 15:12:17 -0300

    Update input-select.blade.php

--- resources/views/components/input-select.blade.php
+++ resources/views/components/input-select.blade.php
@@ -1,7 +1,6 @@
-<select   wire:model.change="{{$wireModel}}" class="block mt-1 border-primary focus:border-indigo-500 rounded-full shadow-sm focus:ring focus:ring-indigo-200 focus:ring-opacity-50 xl:w-[570px] sm:w-[440px] w-[350px] lg:h-[50px]
-                     h-[36px]">
-                        <option value="">{{ $firtOption }}</option>
-                        @foreach ($options as $option)
-                            <option value="{{ $option }}">{{ $option }}</option>
-                        @endforeach
-                    </select>
+<select wire:model.change="{{$wireModel}}" class="block mt-1 border-primary focus:border-secondary rounded-full shadow-sm focus:ring focus:ring-indigo-200 focus:ring-opacity-50 xl:w-[570px] sm:w-[440px] w-[350px] lg:h-[50px] h-[36px] font-light ">
+    <option value="">{{ $firtOption }}</option>
+    @foreach ($options as $option)
+        <option value="{{ $option }}">{{ $option }}</option>
+    @endforeach
+</select>
