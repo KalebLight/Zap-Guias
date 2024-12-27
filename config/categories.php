cd53c702b2582699078267cde173ded01afb93ed
KalebLight <kaleb.luiz00@gmail.com>
2024-12-27 15:31:27 -0300

    Create categories.php

---
+++ config/categories.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+  'Passeios' => ['Ecoturismo', 'Social', 'Sol e Praia', 'Aventura', 'Cultural', 'Rural', 'Pesca', 'Naútico', 'Negócios e Eventos', 'Estudo e Intercâmbio', 'Esporte', 'Saúde'],
+  'Entretenimento' => ['Cinema', 'Teatro', 'Shows'],
+  'Hospedagem' => ['Hotéis', 'Pousadas', 'Hostels'],
+  'Restaurantes' => ['Italiano', 'Regional', 'Brasileiro', 'Japonês', 'Chinês', 'Francês', 'Espanhol', 'Alemão', 'Mexicano', 'Asiático', 'Marroquino', 'Português', 'Americano', 'Tailandês', 'Argentino', 'Indiano', 'Grego', 'Árabe'],
+  'Transporte' => ['Táxi', 'Ônibus', 'Carro Alugado'],
+];
