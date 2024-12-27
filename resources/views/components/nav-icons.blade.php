aaf5f02271c38194a1cef2ae0d607139e72c76f9
KalebLight <kaleb.luiz00@gmail.com>
2024-12-27 15:32:02 -0300

    using categories file

--- resources/views/components/nav-icons.blade.php
+++ resources/views/components/nav-icons.blade.php
@@ -1,32 +1,10 @@
 <div class="justify-between relative w-full md:px-[100px] lg:px-[188px] sm:px-[60px] flex lg:flex hidden lg:block">
-    @livewire('search-desktop-button', [
-    'iconDefault' => 'images/passeios-off-icon.svg',
-    'iconHover' => 'images/passeios-on-icon.svg',
-    'text' => 'Passeios',
-    'items' => ['Ecoturismo', 'Social', 'Sol e Praia', 'Aventura', 'Cultural', 'Rural', 'Pesca', 'Naútico', 'Negócios e Eventos', 'Estudo e Intercâmbio', 'Esporte', 'Saúde']
-])
-    @livewire('search-desktop-button', [
-    'iconDefault' => 'images/entretenimento-off-icon.svg',
-    'iconHover' => 'images/entretenimento-on-icon.svg',
-    'text' => 'Entretenimento',
-    'items' => ['Cinema', 'Teatro', 'Shows']
-])
-    @livewire('search-desktop-button', [
-    'iconDefault' => 'images/hospedagem-off-icon.svg',
-    'iconHover' => 'images/hospedagem-on-icon.svg',
-    'text' => 'Hospedagem',
-    'items' => ['Hotéis', 'Pousadas', 'Hostels']
-])
-    @livewire('search-desktop-button', [
-    'iconDefault' => 'images/restaurantes-off-icon.svg',
-    'iconHover' => 'images/restaurantes-on-icon.svg',
-    'text' => 'Restaurantes',
-    'items' => ['Italiano', 'Regional', 'Braisileiro', 'Japonês', 'Chinês', 'Francês', 'Espanhol', 'Alemão', 'Mexicano', 'Asiático', 'Marroquino', 'Português', 'Americano', 'Tailandês', 'Argentino', 'Indiano', 'Grego', 'Árabe',]
-])
-    @livewire('search-desktop-button', [
-    'iconDefault' => 'images/transporte-off-icon.svg',
-    'iconHover' => 'images/transporte-on-icon.svg',
-    'text' => 'Transporte',
-    'items' => ['Táxi', 'Ônibus', 'Carro Alugado']
-])
+    @foreach (config('categories') as $text => $items)
+        @livewire('search-desktop-button', [
+            'iconDefault' => "images/" . strtolower($text) . "-off-icon.svg",
+            'iconHover' => "images/" . strtolower($text) . "-on-icon.svg",
+            'text' => $text,
+            'items' => $items,
+        ])
+    @endforeach
 </div>
