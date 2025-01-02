9bfea423236d23a8d8cf008db7bf60e9a4ff9a46
KalebLight <kaleb.luiz00@gmail.com>
2025-01-02 17:59:51 -0300

    Update nav-icons.blade.php

--- resources/views/components/nav-icons.blade.php
+++ resources/views/components/nav-icons.blade.php
@@ -1,4 +1,4 @@
-<div class="justify-between relative w-full md:px-[100px] lg:px-[188px] sm:px-[60px] flex lg:flex hidden lg:block">
+<div class="justify-between relative w-full  flex lg:flex hidden lg:block">
     @foreach (config('categories') as $text => $items)
         @livewire('search-desktop-button', [
             'iconDefault' => "images/" . strtolower($text) . "-off-icon.svg",
