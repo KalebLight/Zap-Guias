98846ad62a383bcd039054f0390e20e1370e8295
KalebLight <kaleb.luiz00@gmail.com>
2025-01-08 21:45:07 -0300

    gradient test

--- resources/views/components/hamburguer-menu.blade.php
+++ resources/views/components/hamburguer-menu.blade.php
@@ -14,7 +14,7 @@
         </div>
 
         <!-- SideMenu -->
-        <div class="w-3/4 bg-white overflow-y-auto" x-show="open" x-transition:enter="transition ease-out duration-300 transform" x-transition:enter-start="translate-x-full" x-transition:enter-end="translate-x-0" x-transition:leave="transition ease-in duration-300 transform" x-transition:leave-start="translate-x-0" x-transition:leave-end="translate-x-full">
+        <div class="w-3/4 overflow-y-auto" style="background-image: linear-gradient(to left, #ffffff 85%, rgba(255, 255, 255, 0) 100%)" x-show="open" x-transition:enter="transition ease-out duration-300 transform" x-transition:enter-start="translate-x-full" x-transition:enter-end="translate-x-0" x-transition:leave="transition ease-in duration-300 transform" x-transition:leave-start="translate-x-0" x-transition:leave-end="translate-x-full">
 
             <!-- Sidemenu content -->
             <div class="pt-7 px-4 pb-4">
