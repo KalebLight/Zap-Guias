44dc2e0cd68621eae4362fa7df7f18515dacb849
KalebLight <kaleb.luiz00@gmail.com>
2025-01-13 11:27:46 -0300

    add issets and conditions

--- resources/views/components/partner-profile/info-payment.blade.php
+++ resources/views/components/partner-profile/info-payment.blade.php
@@ -1,21 +1,32 @@
 <div class="lg:mt-16">
   <p class="lg:partner-label text-sm underline font-medium">Informações</p>
   <p class="partner-info lg:block hidden ">Preço: $$</p>
-  <div class="mt-1">
+  @isset($partner->funcionamento)
+    <div class="mt-1">
     <p class="lg:partner-info text-sm font-medium">Funcionamento:</p>
     @foreach(formatarHorario(json_decode($partner->funcionamento, true)) as $horario)
     <p class="lg:partner-info text-sm font-medium">{{ $horario['dias'] }} {{ $horario['horario'] }}</p>
   @endforeach
-  </div>
+    </div>
+  @endisset
   <!-- formas de pagamento -->
   <div class=" mt-1 pb-3 lg:border-b lg:border-primary">
-    <p class="lg:partner-info text-sm font-medium">Formas de pagamento:</p>
-    <p class="lg:partner-info text-sm font-medium">
-      @foreach(json_decode($partner->formas_de_pagamento, true) as $forma => $ativa)
+    @php
+    $formasPagamento = json_decode($partner->formas_de_pagamento, true);
+  @endphp
+
+    @if(array_filter($formasPagamento))
+    <div>
+      <p class="lg:partner-info text-sm font-medium">Formas de pagamento:</p>
+      <p class="lg:partner-info text-sm font-medium">
+      @foreach($formasPagamento as $forma => $ativa)
       @if($ativa)
       {{ ucfirst($forma) }}@if(!$loop->last), @endif
     @endif
     @endforeach
-    </p>
+      </p>
+    </div>
+  @endif
+
   </div>
 </div>
