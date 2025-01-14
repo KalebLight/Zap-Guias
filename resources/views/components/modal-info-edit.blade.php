973ecc62bd2e353df24260bede29dc74c6d1f129
KalebLight <kaleb.luiz00@gmail.com>
2025-01-14 14:34:02 -0300

    add tailwind scrollbar

--- resources/views/components/modal-info-edit.blade.php
+++ resources/views/components/modal-info-edit.blade.php
@@ -8,14 +8,14 @@
     <!-- Modal Container -->
     <div class="relative flex flex-col w-full max-w-md mx-auto bg-white rounded-lg shadow-lg overflow-hidden" style="max-height: calc(100vh - 64px); margin-top: 32px; margin-bottom: 32px;">
       <!-- Close Button -->
-      <button type="button" wire:click="closeModal" class="absolute top-3 right-3 text-gray-400 hover:text-gray-600 transition duration-150 ease-in-out">
+      <button type="button" wire:click="closeModal" class="absolute top-3 right-3 text-gray-400 hover:text-gray-600 transition duration-150 ease-in-out bg-white">
       <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
       </svg>
       </button>
 
       <!-- Modal Content with Scroll -->
-      <div class="flex-1 overflow-y-auto px-5 pt-5 pb-5">
+      <div class="flex-1 overflow-y-auto overflow-x-scroll px-5 pt-5 pb-5 scrollbar scrollbar-thumb-secondary scrollbar-track-secondary scrollbar-always">
       <!-- Social Media -->
       <div class="mb-4">
         <h4 class="underline text-primary text-left font-medium">Redes Sociais</h4>
