00f8a745f3b9087998c348a889f4495ec0078ceb
KalebLight <kaleb.luiz00@gmail.com>
2025-01-18 15:09:06 -0300

    add italic to address

--- resources/views/components/partner-profile/address.blade.php
+++ resources/views/components/partner-profile/address.blade.php
@@ -1,7 +1,7 @@
 @if ($partner->endereco)
   <div class="lg:mt-2">
     <a href="https://www.google.com/maps/search/{{$endereco->logradouro}},+{{$endereco->numero}}+-+{{$endereco->bairro}},+{{$endereco->cidade}}+-+{{$endereco->estado}}+{{$endereco->cep}}" target="_blank">
-    <p class="leading-none text-primary font-medium lg:no-underline hover:underline lg:text-left underline text-right">
+    <p class="leading-none text-primary font-medium lg:no-underline hover:underline lg:text-left underline text-right italic">
       {{$endereco->logradouro}}, {{$endereco->numero}} - {{$endereco->bairro}}<br>
       {{$endereco->cidade}} - {{$endereco->estado}}<br>
       CEP: {{$endereco->cep}}
