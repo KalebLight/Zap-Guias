e60192423d09e2dcbe0c97951d14aa5258ac503f
KalebLight <kaleb.luiz00@gmail.com>
2025-01-18 14:51:46 -0300

    Update modal-info-edit.blade.php

--- resources/views/components/modal-info-edit.blade.php
+++ resources/views/components/modal-info-edit.blade.php
@@ -6,7 +6,7 @@
     <div class="absolute inset-0 bg-gray-400 opacity-55 transition-opacity" wire:click="closeModal"></div>
 
     <!-- Modal Container -->
-    <div class="relative flex flex-col w-full max-w-md mx-auto bg-white rounded-lg shadow-lg overflow-hidden" style="max-height: calc(100vh - 64px); margin-top: 32px; margin-bottom: 32px;">
+    <div class="relative flex flex-col w-full max-w-md mx-auto bg-white rounded-lg shadow-lg overflow-hidden" style="max-height: calc(100vh - 144px); margin-top: 32px; margin-bottom: 32px;">
       <!-- Close Button -->
       <button type="button" wire:click="closeModal" class="absolute top-3 right-3 text-gray-400 hover:text-gray-600 transition duration-150 ease-in-out bg-white">
       <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
