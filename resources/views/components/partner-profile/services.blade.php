afbcd35aa9ffb277672912fb20fbae1671954bbe
KalebLight <kaleb.luiz00@gmail.com>
2025-01-22 14:48:41 -0300

    Update services.blade.php

--- resources/views/components/partner-profile/services.blade.php
+++ resources/views/components/partner-profile/services.blade.php
@@ -1,6 +1,14 @@
 <div class="mt-4">
-  <h2 class="text-primary font-black lg:text-5xl text-3xl lg:mt-1 lg:p-0 pt-2 mb-5  border-t border-primary">{{getServicosLabel($class)}}</h2>
 
+  <div class="w-full flex flex-row justify-between mb-2  border-t border-primary">
+    <h2 class="text-primary  font-black lg:text-5xl text-3xl lg:mt-1 lg:p-0 pt-2 mb-5 ">{{getServicosLabel($class)}}</h2>
+    @if ($isOwner)
+    <x-primary-button width="" class="underline mt-2 h-1/5" wire:click="$dispatch('openAddressModal')">
+      Adicionar Serviços
+    </x-primary-button>
+
+  @endif
+  </div>
   <!-- servicos -->
   <div class="flex flex-row flex-wrap w-full gap-1 justify-center">
     @foreach ($servicos as $servico)
