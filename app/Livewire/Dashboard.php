472f7550389cb3c524840626ece9f2142ee3495d
KalebLight <kaleb.luiz00@gmail.com>
2025-01-22 11:56:26 -0300

    eh gambiarra, tem jeito não

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -2,6 +2,15 @@
 
 namespace App\Livewire;
 
+use App\Models\AcampamentoTuristico;
+use App\Models\AgenciasDeTurismo;
+use App\Models\CasaDeEspetaculos;
+use App\Models\CentroDeConvencoes;
+use App\Models\GuiaDeTurismo;
+use App\Models\LocadoraDeVeiculosParaTuristas;
+use App\Models\MeioDeHospedagem;
+use App\Models\ParqueAquaticoEEmpreendimentoDeLazer;
+use App\Models\ParqueTematico;
 use App\Models\Restaurante;
 use App\Models\TurismoNautico;
 use Livewire\Component;
@@ -18,7 +27,7 @@ class Dashboard extends Component
 
     public function loadPartners()
     {
-        $this->partners = collect()->merge(Restaurante::all())->merge(TurismoNautico::all());
+        $this->partners = collect()->merge(Restaurante::all())->merge(TurismoNautico::all())->merge(GuiaDeTurismo::all())->merge(CentroDeConvencoes::all())->merge(CasaDeEspetaculos::all())->merge(AcampamentoTuristico::all())->merge(LocadoraDeVeiculosParaTuristas::all())->merge(AgenciasDeTurismo::all())->merge(ParqueAquaticoEEmpreendimentoDeLazer::all()->merge(ParqueTematico::all())->merge(MeioDeHospedagem::all()));
     }
 
     public function render()
