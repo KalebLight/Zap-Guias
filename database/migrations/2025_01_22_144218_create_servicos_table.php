cdefe0b6e78b55f0ca053e2f76af800d93709e52
KalebLight <kaleb.luiz00@gmail.com>
2025-01-22 11:54:44 -0300

    add servicos migrations

---
+++ database/migrations/2025_01_22_144218_create_servicos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('servicos', function (Blueprint $table) {
+            $table->id();
+            $table->morphs('empresa');
+            $table->string('titulo');
+            $table->text('descricao')->nullable();
+            $table->decimal('preco', 10, 2);
+            $table->string('local')->nullable();
+            $table->text('inclui')->nullable();
+            $table->string('foto')->nullable();
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('servicos');
+    }
+};
