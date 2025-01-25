897b1e09e99591e582dc054b60de779c34273d53
KalebLight <kaleb.luiz00@gmail.com>
2025-01-25 16:47:37 -0300

    add expendable divs

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -12,6 +12,9 @@
                 <p class="text-primary font-black text-8xl">PLANEJE</p>
                 <p class="text-primary font-black text-8xl">SUAS</p>
                 <p class="text-secondary font-black text-8xl">FÉRIAS</p>
+                <div class="w-full flex justify-end mt-3">
+                    <x-search-input id="search" placeholder='Pesquisar' class="block w-max shadow-custom placeholder-secondary" type="email" name="search" required autofocus />
+                </div>
             </div>
         </main>
     </div>
@@ -45,4 +48,59 @@
             @endforelse
         </tbody>
     </table>
-</div>
+    <p class="text-primary font-black text-3xl mt-20">TEM DÚVIDAS?</p>
+    <div class="w-full">
+
+
+        <!-- Conteúdo expansível -->
+        
+    <div x-data="{ expanded: false }" @click="expanded = !expanded" class="w-full bg-buttonPrimary shadow-custom h-fit px-4 rounded-3xl">
+        <div style="height: 10px;"></div>
+        <div class="flex flex-row items-center">
+            <svg class="w-6 h-6 ml-2 text-secondary transition-transform duration-300 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" :class="{ 'rotate-45': expanded }">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"/>
+            </svg>
+            <p class="text-primary">Encontrei minha empresa listada, o que faço?</p>    
+        </div>
+
+        <div style="height: 10px;"></div>
+
+        <div x-show="expanded" x-collapse class="pl-7">
+            <p class="p-4"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor. </p>
+        </div>
+    </div>
+    
+    <div x-data="{ expanded: false }" @click="expanded = !expanded" class="w-full bg-buttonPrimary shadow-custom h-fit px-4 rounded-3xl mt-3">
+        <div style="height: 10px;"></div>
+        <div class="flex flex-row items-center">
+            <svg class="w-6 h-6 ml-2 text-secondary transition-transform duration-300 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" :class="{ 'rotate-45': expanded }">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"/>
+            </svg>
+            <p class="text-primary">Encontrei minha empresa listada, o que faço?</p>    
+        </div>
+
+        <div style="height: 10px;"></div>
+
+        <div x-show="expanded" x-collapse class="pl-7">
+            <p class="p-4"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor. </p>
+        </div>
+    </div>
+    
+    <div x-data="{ expanded: false }" @click="expanded = !expanded" class="w-full bg-buttonPrimary shadow-custom h-fit px-4 rounded-3xl mt-3">
+        <div style="height: 10px;"></div>
+        <div class="flex flex-row items-center">
+            <svg class="w-6 h-6 ml-2 text-secondary transition-transform duration-300 mr-3" fill="none" stroke="currentColor" viewBox="0 0 24 24" :class="{ 'rotate-45': expanded }">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"/>
+            </svg>
+            <p class="text-primary">Encontrei minha empresa listada, o que faço?</p>    
+        </div>
+
+        <div style="height: 10px;"></div>
+
+        <div x-show="expanded" x-collapse class="pl-7">
+            <p class="p-4"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus imperdiet, nulla et dictum interdum, nisi lorem egestas odio, vitae scelerisque enim ligula venenatis dolor. </p>
+        </div>
+    </div>
+
+
+    </div>
