6271b98f0aea272758efa2de18d95fca5c9fc5d5
KalebLight <kaleb.luiz00@gmail.com>
2025-02-05 15:17:39 -0300

    add compare buttons

--- resources/views/livewire/favoritos-page.blade.php
+++ resources/views/livewire/favoritos-page.blade.php
@@ -2,9 +2,17 @@
     <div class="w-full border-t border-t-primary pt-2">
         <h3 class="text-primary font-black lg:text-7xl xs:text-5xl text-4xl text-right mb-5 break-words">{{Auth::user()->name}}</h3>
 
-        <div class="mt-10 mb-2">
-            <h4 class="text-primary font-black text-4xl">Passeios/Serviços</h4>
-            <h4 class="text-primary font-black text-4xl">Favoritos</h4>
+        <!-- Passeios / Serviços -->
+        <div class="mt-10 mb-2 flex flex-row justify-between items-end">
+            <div>
+                <h4 class="text-primary font-black text-4xl">Passeios/Serviços</h4>
+                <h4 class="text-primary font-black text-4xl">Favoritos</h4>
+
+            </div>
+            <!-- Botão compare 'servicos' p/ telas grandes -->
+            <button class="sm:block hidden text-primary font-semibold text-lg underline h-fit px-1 hover:bg-gray-200 rounded-md">
+                COMPARE
+            </button>
         </div>
 
         <div class="flex overflow-x-auto sm:flex-wrap sm:justify-between sm:gap-2 gap-5 pb-3">
@@ -15,9 +23,25 @@
             @endforeach
         </div>
 
-        <div class="mt-10 mb-2">
-            <h4 class="text-primary font-black text-4xl">Parceiros</h4>
-            <h4 class="text-primary font-black text-4xl">Favoritos</h4>
+        <!-- Botão compare 'servicos' p/ telas pequenas -->
+        <div class="w-full flex flex-row justify-end">
+            <button class="sm:hidden block text-primary font-semibold text-lg underline h-fit px-1 hover:bg-gray-200 rounded-md">
+                COMPARE
+            </button>
+        </div>
+
+
+        <!-- Parceiros -->
+        <div class="mt-10 mb-2 flex flex-row justify-between items-end">
+            <div>
+                <h4 class="text-primary font-black text-4xl">Parceiros</h4>
+                <h4 class="text-primary font-black text-4xl">Favoritos</h4>
+            </div>
+
+            <!-- Botão compare 'parceiros' p/ telas grandes -->
+            <button class="sm:block hidden text-primary font-semibold text-lg underline h-fit px-1 hover:bg-gray-200 rounded-md">
+                COMPARE
+            </button>
         </div>
 
         <div class="flex overflow-x-auto sm:flex-wrap sm:justify-between sm:gap-2 gap-5 pb-3">
@@ -27,10 +51,12 @@
                 </div>
             @endforeach
         </div>
-
-
-        <div>
-
+        <div class="w-full flex flex-row justify-end">
+            <button class="sm:hidden block text-primary font-semibold text-lg underline h-fit px-1 hover:bg-gray-200 rounded-md">
+                COMPARE
+            </button>
         </div>
+
+
     </div>
 </div>
